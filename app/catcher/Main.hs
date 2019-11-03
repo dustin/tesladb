245132ea76317ff6c96e99a1df0b1b18c5bfad33
Dustin Sallings <dustin@spy.net>
2019-11-02 22:23:27 -0700

    catcher syncing data via mqtt rpc

--- app/catcher/Main.hs
+++ app/catcher/Main.hs
@@ -3,22 +3,37 @@
 
 module Main where
 
-import           Data.Maybe             (fromJust)
-import           Data.Text              (Text)
-import           Data.Time.Format       (defaultTimeLocale, formatTime)
-import           Data.Time.LocalTime    (getCurrentTimeZone, utcToLocalTime)
-import           Data.Word              (Word32)
-import           Database.SQLite.Simple hiding (bind, close)
+import           Control.Concurrent.STM     (atomically, newTChanIO, readTChan,
+                                             writeTChan)
+import qualified Control.Exception          as E
+import           Control.Monad              ((<=<))
+import           Data.Aeson                 (decode, encode)
+import qualified Data.ByteString.Lazy       as BL
+import qualified Data.ByteString.Lazy.Char8 as BC
+import           Data.Map.Strict            (Map)
+import qualified Data.Map.Strict            as Map
+import           Data.Maybe                 (fromJust)
+import qualified Data.Set                   as Set
+import           Data.Text                  (Text)
+import qualified Data.Text.Encoding         as TE
+import           Data.Time.Clock            (UTCTime)
+import           Data.Time.Format           (defaultTimeLocale, formatTime)
+import           Data.Time.LocalTime        (getCurrentTimeZone, utcToLocalTime)
+import qualified Data.UUID                  as UUID
+import           Data.Word                  (Word32)
+import           Database.SQLite.Simple     hiding (bind, close)
 import           Network.MQTT.Client
-import           Network.MQTT.Types     (RetainHandling (..))
+import           Network.MQTT.Types         (RetainHandling (..))
 import           Network.URI
-import           Options.Applicative    (Parser, auto, execParser, fullDesc,
-                                         help, helper, info, long, maybeReader,
-                                         option, progDesc, showDefault,
-                                         strOption, switch, value, (<**>))
-import           System.Log.Logger      (Priority (DEBUG), debugM,
-                                         rootLoggerName, setLevel,
-                                         updateGlobalLogger)
+import           Options.Applicative        (Parser, auto, execParser, fullDesc,
+                                             help, helper, info, long,
+                                             maybeReader, option, progDesc,
+                                             showDefault, strOption, switch,
+                                             value, (<**>))
+import           System.Log.Logger          (Priority (DEBUG), debugM,
+                                             rootLoggerName, setLevel,
+                                             updateGlobalLogger)
+import           System.Random              (randomIO)
 
 import           Tesla
 import           TeslaDB
@@ -31,6 +46,9 @@ data Options = Options {
   , optCleanSession :: Bool
   }
 
+blToText :: BL.ByteString -> Text
+blToText = TE.decodeUtf8 . BL.toStrict
+
 options :: Parser Options
 options = Options
   <$> strOption (long "dbpath" <> showDefault <> value "tesla.db" <> help "tesladb path")
@@ -39,8 +57,73 @@ options = Options
   <*> option auto (long "session-expiry" <> showDefault <> value 3600 <> help "Session expiration")
   <*> switch (long "clean-session" <> help "Clean the MQTT session")
 
+type Callback = MQTTClient -> Topic -> BL.ByteString -> [Property] -> IO ()
+
+withMQTT :: Options -> Callback -> (MQTTClient -> IO ()) -> IO ()
+withMQTT Options{..} cb = E.bracket conn normalDisconnect
+  where
+    conn = do
+      mc <- connectURI mqttConfig{_cleanSession=False,
+                                  _protocol=Protocol50,
+                                  _msgCB=SimpleCallback cb,
+                                  _connProps=[PropReceiveMaximum 65535,
+                                              PropSessionExpiryInterval optSessionTime,
+                                              PropTopicAliasMaximum 10,
+                                              PropRequestResponseInformation 1,
+                                              PropRequestProblemInformation 1]}
+            optMQTTURI
+      props <- svrProps mc
+      debugM rootLoggerName $ mconcat ["MQTT connected: ", show props]
+      pure mc
+
+mqttRPC :: MQTTClient -> Topic -> BL.ByteString -> IO BL.ByteString
+mqttRPC mc topic req = do
+  r <- newTChanIO
+  corr <- BL.fromStrict . UUID.toASCIIBytes <$> randomIO
+  subid <- BL.fromStrict . ("$rpc/" <>) . UUID.toASCIIBytes <$> randomIO
+  go corr subid r
+
+  where go theID theTopic r = E.bracket reg unreg call
+          where reg = do
+                  atomically $ registerCorrelated mc theID (SimpleCallback cb)
+                  subscribe mc [(blToText theTopic, subOptions)] mempty
+                unreg _ = do
+                  atomically $ unregisterCorrelated mc theID
+                  unsubscribe mc [blToText theTopic] mempty
+                cb _ _ m _ = atomically $ writeTChan r m
+                call _ = do
+                  publishq mc topic req False QoS2 [
+                    PropCorrelationData theID,
+                    PropResponseTopic theTopic]
+                  atomically $ readTChan r
+
+backfill :: Connection -> MQTTClient -> IO ()
+backfill db mc = do
+  Just rdays <- decode <$> mqttRPC mc "tesla/x/in/days" "" :: IO (Maybe (Map String Int))
+  ldays <- Map.fromList <$> listDays db
+  let dayDiff = Map.keys $ Map.differenceWith (\a _ -> Just a) rdays ldays
+
+  mapM_ doDay (reverse dayDiff)
+
+    where
+      doDay d = do
+        Just rday <- decode <$> mqttRPC mc "tesla/x/in/day" (BC.pack d) :: IO (Maybe [UTCTime])
+        lday <- Set.fromList <$> listDay db d
+        let rdaySet = Set.fromList rday
+            diff = Set.difference rdaySet lday
+
+        mapM_ doOne $ Set.toList diff
+
+      doOne ts = do
+        let (Just k) = (inner . encode) ts
+        debugM rootLoggerName $ mconcat ["Fetching remote data from ", show ts]
+        vd <- mqttRPC mc "tesla/x/in/fetch" k
+        insertVData db vd
+
+          where inner = BL.stripPrefix "\"" <=< BL.stripSuffix "\""
+
 run :: Options -> IO ()
-run Options{..} = do
+run opts@Options{..} = do
   updateGlobalLogger rootLoggerName (setLevel DEBUG)
 
   withConnection optDBPath storeThings
@@ -55,21 +138,13 @@ run Options{..} = do
     storeThings db = do
       dbInit db
 
-      mc <- connectURI mqttConfig{_cleanSession=False,
-                                  _protocol=Protocol50,
-                                  _msgCB=SimpleCallback (sink db),
-                                  _connProps=[PropReceiveMaximum 65535,
-                                              PropSessionExpiryInterval optSessionTime,
-                                              PropTopicAliasMaximum 10,
-                                              PropRequestResponseInformation 1,
-                                              PropRequestProblemInformation 1]}
-            optMQTTURI
-      props <- svrProps mc
-      debugM rootLoggerName $ mconcat ["MQTT connected: ", show props]
-      subr <- subscribe mc [(optMQTTTopic, subOptions{_subQoS=QoS2, _retainHandling=SendOnSubscribeNew})] mempty
-      debugM rootLoggerName $ mconcat ["Sub response: ", show subr]
+      withMQTT opts (sink db) $ \mc -> do
+        subr <- subscribe mc [(optMQTTTopic, subOptions{_subQoS=QoS2, _retainHandling=SendOnSubscribeNew})] mempty
+        debugM rootLoggerName $ mconcat ["Sub response: ", show subr]
 
-      waitForClient mc
+        backfill db mc
+
+        waitForClient mc
 
 main :: IO ()
 main = run =<< execParser opts
