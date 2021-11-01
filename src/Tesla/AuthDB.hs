bdba4c404425402ae03607870714d525b466975c
Dustin Sallings <dustin@spy.net>
2021-10-31 18:00:37 -0700

    Clean up AuthDB a bit

--- src/Tesla/AuthDB.hs
+++ src/Tesla/AuthDB.hs
@@ -1,40 +1,28 @@
+{-# OPTIONS_GHC -Wno-orphans #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards   #-}
 
 module Tesla.AuthDB (updateAuth, loadAuth, loadAuthInfo) where
 
-import           Control.Monad          (guard)
 import           Database.SQLite.Simple hiding (bind, close)
 
 import           Tesla.Auth             (AuthInfo (..), AuthResponse (..), fromToken)
 
-createStatement :: Query
-createStatement = "create table if not exists authinfo (ts, access_token, refresh_token, expires_in)"
+instance ToRow AuthResponse where
+  toRow (AuthResponse tok expiry refresh) = toRow (tok, expiry, refresh)
 
-insertStatement :: Query
-insertStatement = "insert into authinfo(ts, access_token, refresh_token, expires_in) values(current_timestamp, ?, ?, ?)"
-
-deleteStatement :: Query
-deleteStatement = "delete from authinfo"
-
-selectStatement :: Query
-selectStatement = "select access_token, refresh_token, expires_in from authinfo"
+instance FromRow AuthResponse where
+  fromRow = AuthResponse <$> field <*> field <*> field
 
 updateAuth :: FilePath -> AuthResponse -> IO ()
-updateAuth dbPath AuthResponse{..} = withConnection dbPath up
-  where up db = do
-          execute_ db createStatement
-          withTransaction db $ do
-            execute_ db deleteStatement
-            execute db insertStatement (_access_token, _refresh_token, _expires_in)
+updateAuth dbPath ar = withConnection dbPath $ \db -> do
+  execute_ db "create table if not exists authinfo (ts, access_token, refresh_token, expires_in)"
+  withTransaction db $ do
+    execute_ db "delete from authinfo"
+    execute db "insert into authinfo(ts, access_token, refresh_token, expires_in) values(current_timestamp, ?, ?, ?)" ar
 
 loadAuth :: FilePath -> IO AuthResponse
-loadAuth dbPath = withConnection dbPath up
-  where up db = do
-          rows <- query_ db selectStatement :: IO [(String, String, Int)]
-          guard (length rows == 1)
-          let [(_access_token, _refresh_token, _expires_in)] = rows
-          pure $ AuthResponse{..}
+loadAuth dbPath = withConnection dbPath $ \db ->
+  head <$> query_ db "select access_token, refresh_token, expires_in from authinfo"
 
 loadAuthInfo :: FilePath -> IO AuthInfo
-loadAuthInfo p = loadAuth p >>= \AuthResponse{..} -> pure $ fromToken _access_token
+loadAuthInfo p = fromToken . _access_token <$> loadAuth p
