f303aba7750492e43c11a928a7a3f8c7a6626ada
Dustin Sallings <dustin@spy.net>
2023-04-13 19:32:02 -1000

    Put the auth fields in the correct order

--- src/Tesla/AuthDB.hs
+++ src/Tesla/AuthDB.hs
@@ -15,14 +15,14 @@ instance FromRow AuthResponse where
 
 updateAuth :: FilePath -> AuthResponse -> IO ()
 updateAuth dbPath ar = withConnection dbPath $ \db -> do
-  execute_ db "create table if not exists authinfo (ts, access_token, refresh_token, expires_in)"
+  execute_ db "create table if not exists authinfo (ts datetime, access_token varchar, refresh_token varchar, expires_in int)"
   withTransaction db $ do
     execute_ db "delete from authinfo"
     execute db "insert into authinfo(ts, access_token, refresh_token, expires_in) values(current_timestamp, ?, ?, ?)" ar
 
 loadAuth :: FilePath -> IO AuthResponse
 loadAuth dbPath = withConnection dbPath $ \db ->
-  head <$> query_ db "select access_token, refresh_token, expires_in from authinfo"
+  head <$> query_ db "select access_token, expires_in, refresh_token from authinfo"
 
 loadAuthInfo :: FilePath -> IO AuthInfo
 loadAuthInfo p = fromToken . _access_token <$> loadAuth p
