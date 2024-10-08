d75758fcb962e5b188ee5dbaedc34fc87a9f0ed0
Dustin Sallings <dustin@spy.net>
2024-10-07 20:19:19 -1000

    Use hasql instead of postgresql-simple

--- src/Tesla/DB/Postgres.hs
+++ src/Tesla/DB/Postgres.hs
@@ -4,67 +4,79 @@
 module Tesla.DB.Postgres (runStr, runConn) where
 
 import           Cleff
-import           Control.Monad                      (guard, void)
-import           Data.String
-import           Data.Time.Clock                    (UTCTime)
-import           Database.PostgreSQL.Simple
-import           Database.PostgreSQL.Simple.FromRow
-import           Database.PostgreSQL.Simple.SqlQQ   (sql)
-import           Database.PostgreSQL.Simple.ToRow
+import           Control.Exception          (throwIO)
+import qualified Data.Aeson                 as J
+import           Data.ByteString.Char8      (pack)
+import qualified Data.ByteString.Lazy       as BL
+import           Data.Foldable              (toList)
+import qualified Data.Text                  as T
+import           Data.Time.Clock            (UTCTime)
+import           Hasql.Connection           (Connection, acquire)
+import           Hasql.Session              (Session, run)
+import qualified Hasql.Session              as Session
+import           Hasql.TH
+import qualified Hasql.Transaction          as TX
+import           Hasql.Transaction.Sessions (transaction)
+import qualified Hasql.Transaction.Sessions as TX
 
-import           Tesla.Auth                         (AuthResponse (..))
-import           Tesla.Car                          (VehicleData, teslaTS)
+import           Tesla.Auth                 (AuthResponse (..))
+import           Tesla.Car                  (VehicleData, teslaTS)
 import           Tesla.DB
 
 runStr :: IOE :> es => String -> Eff (DB : es) a -> Eff es a
-runStr s f = liftIO (connectPostgreSQL (fromString s)) >>= flip runConn f
+runStr s f = liftIO (acquire (pack s)) >>= either (error . show) (flip runConn f)
 
-runConn :: (IOE :> es) => Connection -> Eff (DB : es) a -> Eff es a
+runConn :: forall es a. (IOE :> es) => Connection -> Eff (DB : es) a -> Eff es a
 runConn db = interpret \case
-  InitDB            -> liftIO $ pdbInit db
-  InsertVData vdata -> liftIO $ pinsertVData db vdata
-  ListDays          -> liftIO $ plistDays db
-  ListDay d         -> liftIO $ plistDay db d
-  FetchDatum t      -> liftIO $ pfetchDatum db t
-  UpdateAuth ar     -> liftIO $ pupdateAuth db ar
-  LoadAuth          -> liftIO $ ploadAuth db
+  InitDB            -> rundb pdbInit
+  InsertVData vdata -> rundb $ pinsertVData vdata
+  ListDays          -> rundb plistDays
+  ListDay d         -> rundb $ plistDay d
+  FetchDatum t      -> rundb $ pfetchDatum t
+  UpdateAuth ar     -> rundb $ pupdateAuth ar
+  LoadAuth          -> rundb ploadAuth
 
-pdbInit :: Connection -> IO ()
-pdbInit db = void $ execute_ db [sql|
-                                   create table if not exists data (
-                                     ts timestamp not null primary key,
-                                     vts timestamptz generated always as (to_timestamp(cast (data->'vehicle_state'->>'timestamp' as real) / 1000.0) at time zone 'US/Pacific') stored,
-                                     data jsonb not null
-                                   );
-                                   create table if not exists authinfo (ts timestamp, access_token varchar, refresh_token varchar, expires_in int);
-                                   |]
+  where
+    rundb :: Session b -> Eff es b
+    rundb f = liftIO $ either throwIO pure =<< run f db
 
-pinsertVData :: Connection -> VehicleData -> IO ()
-pinsertVData db vdata = void $ execute db "insert into data(ts, data) values(?, ?)" (teslaTS vdata, vdata)
+pdbInit :: Session ()
+pdbInit = do
+  Session.sql "create table if not exists data (ts timestamp not null primary key, vts timestamptz generated always as (to_timestamp(cast (data->'vehicle_state'->>'timestamp' as real) / 1000.0) at time zone 'US/Pacific') stored, data jsonb not null);"
+  Session.sql "create table if not exists authinfo (ts timestamp, access_token varchar, refresh_token varchar, expires_in int);"
 
-plistDays :: Connection -> IO [(String,Int)]
-plistDays db = query_ db "select date(ts)::text as day, count(*) from data group by day"
+pinsertVData :: VehicleData -> Session ()
+pinsertVData vdata = Session.statement (teslaTS vdata, J.decode vdata) st
+  where
+    st = [resultlessStatement|insert into data (ts, data) values ($1 :: timestamptz, $2 :: jsonb?)|]
 
-plistDay :: Connection -> String -> IO [UTCTime]
-plistDay db d = fmap fromOnly <$> query db "select ts at time zone 'utc' from data where date(ts) = ?" (Only d)
+plistDays :: Session [(String,Int)]
+plistDays = fmap dec . toList <$> Session.statement () st
+  where
+    dec (a, b) = (T.unpack a, fromIntegral b)
+    st = [vectorStatement|select date(ts) :: text as day, count(*) :: int8 from data group by day|]
 
-pfetchDatum :: Connection -> UTCTime -> IO VehicleData
-pfetchDatum db t = do
-  rows <- query db "select data from data where ts = ?" (Only t)
-  guard $ length rows == 1
-  pure . fromOnly . head $ rows
+plistDay :: String -> Session [UTCTime]
+plistDay d = toList <$> Session.statement (T.pack d) st
+  where
+    st = [vectorStatement|select (ts at time zone 'utc') :: timestamptz from data where date(ts)::text = $1 :: text|]
 
-instance ToRow AuthResponse where
-  toRow (AuthResponse tok expiry refresh) = toRow (tok, refresh, expiry)
+pfetchDatum :: UTCTime -> Session VehicleData
+pfetchDatum t = BL.fromStrict <$> Session.statement t st
+  where
+    st = [singletonStatement|select data :: bytea from data where ts = $1 :: timestamptz|]
 
-instance FromRow AuthResponse where
-  fromRow = AuthResponse <$> field <*> field <*> field
+pupdateAuth :: AuthResponse -> Session ()
+pupdateAuth ar = transaction TX.Serializable TX.Write do
+  TX.statement () del
+  TX.statement (T.pack (_access_token ar), T.pack (_refresh_token ar), fromIntegral (_expires_in ar)) ins
 
-pupdateAuth :: Connection -> AuthResponse -> IO ()
-pupdateAuth db ar =
-  withTransaction db do
-    void $ execute_ db "delete from authinfo"
-    void $ execute db "insert into authinfo(ts, access_token, refresh_token, expires_in) values(current_timestamp, ?, ?, ?)" ar
+  where
+    del = [resultlessStatement|delete from authinfo|]
+    ins = [resultlessStatement|insert into authinfo (ts, access_token, refresh_token, expires_in) values(current_timestamp, $1 :: text, $2 :: text, $3 :: int8)|]
 
-ploadAuth :: Connection -> IO AuthResponse
-ploadAuth db = head <$> query_ db "select access_token, expires_in, refresh_token from authinfo"
+ploadAuth :: Session AuthResponse
+ploadAuth = dec <$> Session.statement () st
+  where
+    dec (a, b, c) = AuthResponse (T.unpack a) (fromIntegral b) (T.unpack c)
+    st = [singletonStatement|select access_token :: text, expires_in :: int8, refresh_token :: text from authinfo|]
